7d25030d5e7ae6529c424653224a948084b91e7b
Johnson (Xiuping) Wu <wuxiuping96@gmail.com>
2024-12-13 16:21:55 -0400

    Update on portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,52 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <section className="mb-5">
+      <Image
+        src="/profile/me_placeholder.jpg" 
+        width={100}
+        height={100}
+        className="rounded-full"
+        alt={"This is my profile Image"}      
+      />
+      <h1 className="text-2xl font-bold">Xiuping(Johnson) Wu</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <div className="text-gray-700 dark:text-gray-300">
+          <p className="mt-4">
+            I’m a fullstack software engineer specializing in building scalable
+            web applications having rich user interface using javascript and React.js.
+          </p>
+          <p className="mt-4 mb-4">
+            Over the years, I have worked with a rapidly growing maritime startup, where I played a key role in building end-to-end products for the road construction industry, 
+            with a focus on developing ERP systems. I have also been an active contributor to various open-source projects, gaining valuable experience in collaborative development.
+            <br />
+            <br />
+            Currently, I am seeking new opportunities while maintaining the official website for a globally recognized TIG automatic welding machine company. 
+            My responsibilities include ongoing website maintenance, community engagement, and content updates to enhance the company&apos;s online presence.
+          </p>
+
+          <p className="mb-4">
+            If you&apos;d like to collaborate, please&nbsp;
+            <a
+              href="mailto:wuxiuping96@gmail.com"
+              className="border-b inline-block"
+            >
+              send me an email
+            </a>
+            &nbsp;or reach out on any of my social handles.
+          </p>
+
+          <p className="mb-4">
+            If you are interested in the highly efficient and reliable welding machines, please don&apos;t hesitate to
+            <a
+              href="https://www.ludingweld.com"
+              className="border-b inline-block"
+              target="_blank"
+            >
+              &nbsp;Check Our Machines From Jin Luding Welding Co., Ltd
+            </a>
+          </p>
+      </div>
+    </section>
   );
 }
---
+++ src/data/soicalLinks.ts
@@ -0,0 +1,19 @@
+import {
+  GitHubIcon,
+  LinkedInIcon,
+} from '@/components/icons/icons';
+
+const socialLinks = [
+  {
+    source: 'Github',
+    icon: GitHubIcon,
+    href: ''
+  },
+  {
+    source: 'LinkedIn',
+    icon: LinkedInIcon,
+    href: ''
+  }
+];
+
+export default socialLinks;
