663f59723e07b1234cc1771b413505d42a148ecd
Johnson (Xiuping) Wu <wuxiuping96@gmail.com>
2025-08-20 21:06:43 -0600

    update

--- src/components/rating.tsx
+++ src/components/rating.tsx
@@ -1,27 +1,15 @@
-import { CiStar } from "react-icons/ci";
+
 
 interface props {
   skill: string,
   rating: number
 }
 
-export default function Rating({skill, rating}: props) {
-
-  const rates = Array.from({ length : rating}, (_, index) => (
-    <CiStar key={index} color="#FFD700"/>
-  ))
-
-  const blankRates = Array.from({ length : 5 - rating}, (_, index) => (
-    <CiStar key={index}/>
-  ))
+export default function Rating({skill}: props) {
 
   return (
     <div>
       <p>{skill}</p>
-      {/* <div className="flex flex-row">
-        {rates}
-        {blankRates}
-      </div> */}
     </div>
   )
 }
